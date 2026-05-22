8f48c6171a1d1d9f633d326626bb6dd2d1e0a859
Anmol <AmoCreates@outlook.com>
2026-05-22 20:45:18 +0530

    Set baseURL for API to production endpoint

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   // This automatically picks the right URL based on the environment
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://genwebai-1jh4.onrender.com",
 });
 
 // You can also add interceptors here for tokens/auth later
